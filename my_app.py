5382e01c58e4cc286ac103acc8b5105e0ffa7309
zerg959 <zerg959@yandex.ru>
2025-04-19 16:17:52 +0300

    main

--- my_app.py
+++ my_app.py
@@ -3,3 +3,12 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget, QHBoxLayout,
     QVBoxLayout, QLabel, QPushButton
 )
+
+from instr import *
+
+class TestWin(QWidget):
+        def __init__(self):
+            super().__init__()
+            self.show()
+
+        self.main_line = QVBoxLayout()
